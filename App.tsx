d26218a9f292230a701065b3e04d012589969df7
CristianMatias <cristianmatias52@gmail.com>
2021-11-01 14:20:30 +0100

    Definido paquete de estilos de la aplicación

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { mainConnection } from './src/service/MainService';
+import styles from './styles/styles';
 
 export default class App extends React.Component {
   state = { reponse: ""}
@@ -14,21 +15,11 @@ export default class App extends React.Component {
 
   render(){
     return (
-      <View style={styles.container}>
+      <View style={styles.input}>
         <Text>{this.state.reponse}</Text>
         <StatusBar style="auto" />
       </View>
     );
   }
-
   
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
