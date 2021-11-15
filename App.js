37e173f1c007edf2b58c3497b79f15403c2ac2da
CristianMatias <cristianmatias52@gmail.com>
2021-11-15 16:19:20 +0000

    Añadido el resto de pantallas al navegador

--- App.js
+++ App.js
@@ -3,6 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignIn from './src/components/signin/signin';
 import Login from './src/components/login/login';
+import Home from './src/components/home/home';
+import Information from './src/components/information/information';
+import GameProfile from './src/components/profile/gameProfile/gameProfile';
+import UserProfile from './src/components/profile/userProfile/userProfile';
+import VisitorProfile from './src/components/profile/visitorProfile/visitorProfile';
+import Review from './src/components/review/review';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +23,30 @@ export default function App() {
         <Stack.Screen
           name="Registrarse"
           component={SignIn}
+        />
+        <Stack.Screen
+          name="HomeScreen"
+          component={Home}
+        />
+        <Stack.Screen
+          name="Information"
+          component={Information}
+        />
+        <Stack.Screen
+          name="GameProfile"
+          component={GameProfile}
+        />
+        <Stack.Screen
+          name="UserProfile"
+          component={UserProfile}
+        />
+        <Stack.Screen
+          name="VisitorProfile"
+          component={VisitorProfile}
+        />
+        <Stack.Screen
+          name="Review"
+          component={Review}
         />
       </Stack.Navigator>
     </NavigationContainer>
