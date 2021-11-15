3f50a30679c5714d9dae635e83d277e038fd57ff
CristianMatias <cristianmatias52@gmail.com>
2021-11-15 12:27:40 +0000

    Conexión con el servidor para el registro creada

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, Button, Image, Text } from 'react-native';
 import styles from '../../../styles/styles';
+import {signup} from '../../service/UserService'
+import base64 from 'react-native-base64'
 
 export default function SignIn() {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+
+  function signIn(){
+
+    if(password === repeatPassword) {
+      signup(username, email, base64.encode(password))
+      alert('Usuario registrado')
+    }
+    else alert('Las contraseñas no coinciden')
+  }
 
     return (
       <View style={{backgroundColor: '#40A2FF', flex: 1}}>
         <Image source={require('../../../images/icon.png')} style={styles.signinImage}/>
           <View style={styles.signin}>
-          <TextInput style={styles.signinText} placeholder="Nickname"/>
-          <TextInput style={styles.signinText} placeholder="Email"/>
-          <TextInput style={styles.signinText} placeholder="Contraseña"/>
-          <TextInput style={styles.signinText} placeholder="Repetir Contraseña"/>
+          <TextInput 
+                style={styles.signinText} 
+                placeholder="Nickname"
+                onChangeText={(name) => setUsername(name)}
+          />
+          <TextInput 
+                style={styles.signinText} 
+                placeholder="Email"
+                onChangeText={(mail) => setEmail(mail)}
+                />
+          <TextInput 
+                style={styles.signinText} 
+                placeholder="Contraseña" 
+                onChangeText={(pass) => setPassword(pass)}
+                secureTextEntry={true}
+          />
+          <TextInput 
+                style={styles.signinText} 
+                placeholder="Repetir Contraseña"
+                onChangeText={(pass) => setRepeatPassword(pass)}
+                secureTextEntry={true}
+          />
           <Button color={'#796DF0'} title="Registrarse" 
-          onPress={()=> "Futura llamada al servidor"}/>
+          onPress={()=> signIn()}/>
         </View>
       </View>
       
---
+++ src/service/UserService.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { routes } from "../utils/routes";
+
+const BASE_URL = routes.LOCAL_SERVER + routes.VERSION;
+
+export function signup(username, email, password) {
+    axios({
+        method: "post",
+        url: BASE_URL + routes.SIGNUP,
+        data: {
+            "nickname": username,
+            "email": email,
+            "password": password
+        },
+        headers: { 
+            "Accept": 'application/json',
+            "Content-Type": "application/json"},
+    })
+    .then(response => {return response.data})
+    .catch(err => console.log(err))
+}
+
