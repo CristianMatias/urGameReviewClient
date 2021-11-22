b7686430d8cd60ca418589dc9fc8054c24d6e3b5
CristianMatias <cristianmatias52@gmail.com>
2021-11-22 13:08:08 +0100

    Creado objeto de conexion para logearse

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -2,4 +2,5 @@ export class routes {
     public static readonly LOCAL_SERVER = "http://217.21.78.32:8080"
     public static readonly VERSION = "/v1"
     public static readonly SIGNUP = "/signup"
+    public static readonly LOGIN = '/login'
 }
